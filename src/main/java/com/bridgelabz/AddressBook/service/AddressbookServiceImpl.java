2ecdaf3cf8b75419c397a9303c87af04ac77fa95
erakshaykumar <akaakshay304@gmail.com>
2022-05-10 14:01:34 +0530

    Added: Store AddressBook In List

--- src/main/java/com/bridgelabz/AddressBook/service/AddressbookServiceImpl.java
+++ src/main/java/com/bridgelabz/AddressBook/service/AddressbookServiceImpl.java
@@ -9,36 +9,44 @@ import java.util.List;
 
 @Service
 public class AddressbookServiceImpl implements IAddressBookService {
+
+    List<AddressbookData> addressbookDataList = new ArrayList<>();
+
     @Override
     public List<AddressbookData> getAddressbookData() {
-        List<AddressbookData> addressbookDataList = new ArrayList<>();
-        addressbookDataList.add(new AddressbookData(1, new AddressbookDTO("Akshay", "7057082582")));
         return addressbookDataList;
     }
 
     @Override
     public AddressbookData getAddressbookDataById(int personId) {
         AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(personId, new AddressbookDTO("Akki", "1234567890"));
+        addressbookData = addressbookDataList.get(personId - 1);
         return addressbookData;
     }
 
     @Override
     public AddressbookData createAddressbookData(AddressbookDTO addressbookDTO) {
         AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(1, addressbookDTO);
+        addressbookData = new AddressbookData(addressbookDataList.size() + 1, addressbookDTO);
+        addressbookDataList.add(addressbookData);
         return addressbookData;
     }
 
     @Override
     public AddressbookData updateAddressbookData(int personId, AddressbookDTO addressbookDTO) {
-        AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(personId, addressbookDTO);
+        AddressbookData addressbookData = this.getAddressbookDataById(personId);
+        addressbookData.setName(addressbookDTO.name);
+        addressbookData.setPhNumber(addressbookDTO.phNumber);
+        addressbookDataList.set(personId - 1, addressbookData);
         return addressbookData;
     }
 
     @Override
     public void deleteAddressbookData(int personId) {
-
+        int i = 1;
+        addressbookDataList.remove(personId - 1);
+        for (AddressbookData addressbookData : addressbookDataList) {
+            addressbookData.setPersonId(i++);
+        }
     }
 }
