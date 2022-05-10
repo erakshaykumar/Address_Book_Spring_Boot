f2201fe487c9c447ef779b9210a8e39037f5d7f8
erakshaykumar <akaakshay304@gmail.com>
2022-05-10 14:22:15 +0530

    Added: Lombok For Using Logging SLF4j In Addressbook

--- src/main/java/com/bridgelabz/AddressBook/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/AddressBook/AddressBookApplication.java
@@ -1,14 +1,19 @@
 package com.bridgelabz.AddressBook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book Using Spring Boot");
-		SpringApplication.run(AddressBookApplication.class, args);
+		System.out.println("Welcome to Addressbook App");
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Addressbook App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Addressbook App started");
 	}
 
 }
